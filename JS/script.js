d3681692b8af3dcc282b69251b9261c1f916ceba
Marcelo <joao69@aluno.senai-ce.org.br>
2023-07-09 10:05:55 -0300

    ajeitando o erro

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@ const personagens = document.querySelectorAll('.personagem');
 
 personagens.forEach(personagem => {
     personagem.addEventListener('mouseenter', () => {
-        personagem.classList.add('selecionado');
+        
 
         const personagemSelecionado = document.querySelector('.selecionado');
         personagemSelecionado.classList.remove('selecionado');
