0c18767230d393a6df539438cdbfb191b5a2c48f
Marcelo <joao69@aluno.senai-ce.org.br>
2023-07-09 10:52:08 -0300

    refatorando

--- JS/script.js
+++ JS/script.js
@@ -2,23 +2,42 @@ const personagens = document.querySelectorAll('.personagem');
 
 personagens.forEach(personagem => {
     personagem.addEventListener('mouseenter', () => {
+
+        if(window.innerWidth < 450){
+            window.scrollTo({top :0, behavior: 'smooth'});
+        }
         
 
-        const personagemSelecionado = document.querySelector('.selecionado');
-        personagemSelecionado.classList.remove('selecionado');
+        removerSelecaoDoPersonagem();
         
         personagem.classList.add('selecionado');
 
 
-        const imagemPersonagemGrande = document.querySelector('.personagem-grande');
-        const idPersonagem = personagem.attributes.id.value;
+        alterarImagemPersonagemSelecionado(personagem);
 
-        imagemPersonagemGrande.src =  `../IMG/card-${idPersonagem}.png`;
+        alterarNomePersonagemSelecionado(personagem);
 
-        const nomePersonagem = document.getElementById('nome-personagem');
-        nomePersonagem.innerText = personagem.getAttribute('data-name');
-
-        const descricaoPersonagem = document.getElementById('descricao-personagem');
-        descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+        alterarDescricaoPersonagemSelecionado(personagem);
     })
 })
+
+function alterarDescricaoPersonagemSelecionado(personagem) {
+    const descricaoPersonagem = document.getElementById('descricao-personagem');
+    descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+}
+
+function alterarNomePersonagemSelecionado(personagem) {
+    const nomePersonagem = document.getElementById('nome-personagem');
+    nomePersonagem.innerText = personagem.getAttribute('data-name');
+}
+
+function alterarImagemPersonagemSelecionado(personagem) {
+    const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+    const idPersonagem = personagem.attributes.id.value;
+    imagemPersonagemGrande.src = `../IMG/card-${idPersonagem}.png`;
+}
+
+function removerSelecaoDoPersonagem() {
+    const personagemSelecionado = document.querySelector('.selecionado');
+    personagemSelecionado.classList.remove('selecionado');
+}
